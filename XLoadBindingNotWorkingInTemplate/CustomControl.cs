e7be28e29e1e79765b617faa5572739f5fdfca9f
Roxk <vzox.rock@gmail.com>
2022-05-11 22:09:49 +0800

    Add project files.

---
+++ XLoadBindingNotWorkingInTemplate/CustomControl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace XLoadBindingNotWorkingInTemplate
+{
+    public sealed class CustomControl : Control, INotifyPropertyChanged
+    {
+        public CustomControl()
+        {
+            this.DefaultStyleKey = typeof(CustomControl);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool IsVisible { get; private set; }
+
+        public string Greeting { get { return "Hello world"; } }
+        public void MakeVisible()
+        {
+            IsVisible = true;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsVisible)));
+        }
+    }
+}
